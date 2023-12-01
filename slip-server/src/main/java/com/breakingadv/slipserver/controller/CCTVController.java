23b84ab52b7739f57b06ef29afef5e1848c43ad4
Sauter001 <lsj457@naver.com>
2023-12-01 16:42:29 +0900

    fix: add RequiredArgsConstructor to controller

--- slip-server/src/main/java/com/breakingadv/slipserver/controller/CCTVController.java
+++ slip-server/src/main/java/com/breakingadv/slipserver/controller/CCTVController.java
@@ -1,12 +1,14 @@
 package com.breakingadv.slipserver.controller;
 
-import com.breakingadv.slipserver.service.CCTVStreamingService;
+import com.breakingadv.slipserver.service.CCTVService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/cctvs")
+@RequiredArgsConstructor
 public class CCTVController {
+    private final CCTVService cctvService;
 
 }
--- slip-server/src/main/java/com/breakingadv/slipserver/controller/UserController.java
+++ slip-server/src/main/java/com/breakingadv/slipserver/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.breakingadv.slipserver.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
-
+    private final UserController userController;
 }
