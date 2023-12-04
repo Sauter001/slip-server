885baf58c6dca00915be87a146f398539cc37abd
Sauter001 <lsj457@naver.com>
2023-12-05 00:12:23 +0900

    feat: cctv에 추가 에대한 todo 추가

--- slip-server/src/main/java/com/breakingadv/slipserver/controller/CCTVController.java
+++ slip-server/src/main/java/com/breakingadv/slipserver/controller/CCTVController.java
@@ -1,7 +1,10 @@
 package com.breakingadv.slipserver.controller;
 
 import com.breakingadv.slipserver.model.request.CCTVDuplicationRequest;
+import com.breakingadv.slipserver.model.request.CCTVManageRequest;
 import com.breakingadv.slipserver.model.response.ApiResponse;
+import com.breakingadv.slipserver.model.response.CCTVInfoResponse;
+import com.breakingadv.slipserver.model.response.DuplicationResponse;
 import com.breakingadv.slipserver.service.CCTVService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,13 +19,20 @@ public class CCTVController {
 
     @GetMapping("/cctvIsDuplicated")
     @ResponseBody
-    public ResponseEntity<ApiResponse<Boolean>> getDuplication(@RequestParam String ip) {
-        boolean response = cctvService.getDuplication(ip).isDuplicated();
-
-        if (response) {
-            return ResponseEntity.ok(new ApiResponse<>(true, "IP 중복됨", true));
+    public ResponseEntity<ApiResponse<DuplicationResponse>> getDuplication(@RequestParam String ip) {
+        boolean isDuplicated = cctvService.getDuplication(ip).isDuplicated();
+        DuplicationResponse response = new DuplicationResponse(isDuplicated);
+        if (response.isDuplicated()) {
+            return ResponseEntity.ok(new ApiResponse<>(true, "IP 중복됨", response));
         } else {
-            return ResponseEntity.ok(new ApiResponse<>(true, "IP 중복 안 됨", false));
+            return ResponseEntity.ok(new ApiResponse<>(true, "IP 중복 안 됨", response));
         }
+    }
+
+    @PostMapping("/{userId}/cctvAdd")
+    @ResponseBody
+    public ResponseEntity<ApiResponse<CCTVInfoResponse>> addCCTV(@RequestBody CCTVManageRequest request, @PathVariable String userId) {
+        // TODO: CCTV 추가 기능 구현
+        return ResponseEntity.ok(new ApiResponse<>(true, "예시: " + userId, new CCTVInfoResponse(new String[]{"아빠집", "엄마집"})));
     }
 }
